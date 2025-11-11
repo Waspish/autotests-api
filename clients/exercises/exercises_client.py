fa4d8044f848183d66393c35c9dfbbd9fc6c9d9d
Andrew <ostapenko.andreik@gmail.com>
2025-11-11 15:26:53 +0300

    Клиент упрважнений и get_uuid4 тдля fakers

---
+++ clients/exercises/exercises_client.py
@@ -0,0 +1,86 @@
+from typing import TypedDict
+
+from httpx import Response
+
+from clients.api_client import APIClient
+
+
+class ExerciseClient(APIClient):
+    """
+    Клиент для работы с /api/v1/exercises
+    """
+
+    class CreateExerciseRequestDict(TypedDict):
+        """
+        Описание структуры запроса на создание упражнения для курса.
+        """
+        title: str
+        courseId: str
+        maxScore: int
+        minScore: int
+        orderIndex: int
+        description: str
+        estimatedTime: str
+
+    class UpdateExerciseRequestDict(TypedDict):
+        """
+        Описание структуры запроса на обновление упражнения из курса.
+        """
+        title: str
+        maxScore: int
+        minScore: int
+        orderIndex: int
+        description: str
+        estimatedTime: str
+
+    class GetExercisesRequestDict(TypedDict):
+        """
+        Описание структуры запроса на получение упражнений из курса.
+        """
+        courseId: str
+
+    def create_exercise_api(self, request: CreateExerciseRequestDict) -> Response:
+        """
+        Метод создание упражнения.
+
+        :param request: Словарь с title, courseId, maxScore, minScore, orderIndex, description, estimatedTime.
+        :return: Ответ от сервера в виде объекта httpx.Response
+        """
+        return self.post(url="/api/v1/exercise", json=request)
+
+    def get_exercises_api(self, query: GetExercisesRequestDict) -> Response:
+        """
+        Метод получения упражнений конкретного курса по его uuid.
+
+        :param query: Словарь с courseId.
+        :return: Ответ от сервера в виде объекта httpx.Response
+        """
+        return self.get(url="/api/v1/exercise", params=query)
+
+    def get_exercise_api(self, exercise_id: str) -> Response:
+        """
+        Метод получения упражнения по его идентификатору.
+
+        :param exercise_id: Идентификатор упражнения.
+        :return: Ответ от сервера в виде объекта httpx.Response
+        """
+        return self.get(url=f"/api/v1/exercise/{exercise_id}")
+
+    def delete_exercise_api(self, exercise_id: str) -> Response:
+        """
+        Метод удаления упражнения по его идентификатору.
+
+        :param exercise_id: Идентификатор упражнения.
+        :return: Ответ от сервера в виде объекта httpx.Response
+        """
+        return self.delete(url=f"/api/v1/exercise/{exercise_id}")
+
+    def update_exercise_api(self, exercise_id: str, request: UpdateExerciseRequestDict) -> Response:
+        """
+        Метод обновления упражнения.
+
+        :param request: Словарь с title, maxScore, minScore, orderIndex, description, estimatedTime.
+        :param exercise_id: Идентификатор упражнения.
+        :return: Ответ от сервера в виде объекта httpx.Response
+        """
+        return self.patch(url=f"/api/v1/exercise/{exercise_id}", json=request)
--- tools/fakers.py
+++ tools/fakers.py
@@ -1,5 +1,10 @@
 import time
+import uuid
 
 
 def get_random_email() -> str:
     return f"test.{time.time()}@example.com"
+
+
+def get_uuid4() -> str:
+    return str(uuid.uuid4())
