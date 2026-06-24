a87c752731403d0cf57754a29791f6fb6747b5fe
Andrew <ostapenko.andreik@gmail.com>
2026-06-24 21:17:00 +0300

    added logger and with it new hooks

--- clients/event_hooks.py
+++ clients/event_hooks.py
@@ -1,7 +1,10 @@
 import allure
-from httpx import Request
+from httpx import Request, Response
 
 from tools.http.curl import make_curl_from_request
+from tools.logger import get_logger
+
+logger = get_logger("HTTP_LOGGER")
 
 
 def curl_event_hook(request: Request):
@@ -17,3 +20,11 @@ def curl_event_hook(request: Request):
         name="cURL command",
         attachment_type=allure.attachment_type.TEXT,
     )
+
+
+def log_request_event_hook(request: Request):
+    logger.info(f"Make {request.method} request to {request.url}")
+
+
+def log_response_event_hook(response: Response):
+    logger.info(f"Got response {response.status_code} {response.reason_phrase}")
